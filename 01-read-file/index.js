59cb03f02541c2307cf1bbb771c042e6fe9ff028
Michail <100672207+Michail-Konon@users.noreply.github.com>
2022-05-20 13:26:26 +0300

    fix: fix solution

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,4 +4,8 @@ const path = require('path');
 const setPath = path.join(__dirname, 'text.txt');
 const newStream = fs.createReadStream(setPath, 'utf-8');
 
-newStream.on('data', text => console.log(text));
+let data = '';
+
+newStream.on('data', text => data += text);
+newStream.on('end', () => console.log(data));
+newStream.on('error', error => console.log('Error', error.message));
