acb34c875b2ffd82b0f9b8d558f3692770c9f120
Michail <100672207+Michail-Konon@users.noreply.github.com>
2022-05-23 18:34:12 +0300

    feat: add solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.join(__dirname);
+
+fs.readdir(dir, (err, files) => {
+    if (err) {
+        console.log(err);
+      };
+
+  if (!files.includes('files-copy')) {
+    fs.mkdir(`${dir}/files-copy`, err => {
+        if (err) {
+            console.log(err);
+          };
+    });
+  }
+});
+
+fs.readdir(`${dir}/files`, (err, files) => {
+    if (err) {
+        console.log(err);
+      };
+
+  files.forEach(file => {
+    fs.copyFile(`${dir}/files/${file}`, `${dir}/files-copy/${file}`, err => {
+        if (err) {
+            console.log(err);
+          };
+    });
+  });
+
+  console.log('Done');
+});
